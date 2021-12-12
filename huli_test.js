55eeffb4b915cdf36619aeb5e860d0afa7642e1a
JasonCK <x13244231@gmail.com>
2021-12-13 01:47:41 +0800

    finish BMI calculator

--- huli_test.js
+++ huli_test.js
@@ -1,10 +1,37 @@
-var score = 100;
-var score_result = score >= 60 ? "Pass!" : "Fail!";
-// 判斷 Pass 或 fail
+var body_height = 180 / 100 // 除以 100 將單位從 cm 轉成 m
+var body_weight = 90
+var BMI = parseInt(body_weight / (body_height * body_height)) 
+// BMI 值的計算公式為：體重 / 身高^2。
+// 用 parseInt 轉換成整數，避免小數點影響 if 判斷。
 
-if (score === 100) {
-    console.log("You are No.1!");
-    // 如果是 100 分就顯示 You are No.1!
+
+if (BMI < 18.5) {
+    console.log("體重過輕")
+} else if (18.5 <= BMI && BMI < 24) {
+    console.log("正常範圍")
+} else if (24 <= BMI && BMI < 27) {
+    console.log("過重")
+} else if(27 <= BMI && BMI < 30) {
+    console.log("輕度肥胖")
+} else if(30 <= BMI && BMI < 35){
+    console.log("中度肥胖")
+} else if(35 <= BMI) {
+    console.log("重度肥胖")
+} else {
+    console.log("找不到答案ㄋ")
 }
-else
-    console.log(score_result);
+
+
+// switch(BMI){
+//     case (BMI < 18.5):
+//         console.log("體重過輕")
+//         break
+//     case BMI <= 18.5 && BMI < 24:
+//         console.log("正常範圍")
+//         break
+//     default:
+//         console.log("看到這個表示判斷失敗。")
+//         break
+// }
+
+console.log("BMI number:",BMI)
