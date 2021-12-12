26ad3042e28ce6991cc3d56549d4e0dba4a9200f
JasonCK <x13244231@gmail.com>
2021-12-13 01:05:17 +0800

    add comment

--- huli_test.js
+++ huli_test.js
@@ -1,8 +1,10 @@
 var score = 100;
 var score_result = score >= 60 ? "Pass!" : "Fail!";
+// 判斷 Pass 或 fail
 
 if (score === 100) {
     console.log("You are No.1!");
+    // 如果是 100 分就顯示 You are No.1!
 }
 else
     console.log(score_result);
